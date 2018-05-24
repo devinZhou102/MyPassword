35b14a1b6de4350462858caa91f2907e2f3ccd5f
周志强 <devin_zhou@outlook.com>
2018-05-24 23:18:46 +0800

    1. PwdGenerator

---
+++ src/MyPassword/Helpers/PwdGenerator.cs
@@ -0,0 +1,87 @@
+﻿using MyPassword.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPassword.Utils
+{
+
+    public class PwdGenerator
+    {
+        private readonly string typeNumber = "0123456789";
+        private readonly string typeLetterLowerCase = "abcdefghijkmnopqrstuvwxyz";
+        private readonly string typeLetterUpperCase = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
+        private readonly string typeCharactor = "!@#$%&*=+?";
+
+        static PwdGenerator instance;
+
+        public static PwdGenerator GetInstance()
+        {
+            if (null == instance)
+            {
+                instance = new PwdGenerator();
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// 密码生成
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public string Generate(PwdGeneratorParams param)
+        {
+            StringBuilder password = new StringBuilder();
+            int length = param.Length;
+            if (length < 6)
+            {
+                length = 6;
+            }
+            string seed = GeneratePwdSeed(param);
+            for (int index = 0; index < length; index++)
+            {
+                password.Append(GetPwdCharactor(seed));
+            }
+            return password.ToString();
+        }
+
+        Random rand;
+        private string GetPwdCharactor(string seed)
+        { 
+            int length = seed.Length;
+            if (null == rand)
+            {
+                long tick = DateTime.Now.Ticks;
+                rand = new Random((int)(tick & 0xffffffffL) | (int)(tick >> 32));
+            }
+            int index = rand.Next() % length;
+            return seed.Substring(index,1);
+        }
+
+
+        private string GeneratePwdSeed(PwdGeneratorParams param)
+        {
+            StringBuilder builder = new StringBuilder();
+            if (param.HasNumber)
+            {
+                builder.Append(typeNumber);
+            }
+            if (param.HasLetterLowerCase)
+            {
+                builder.Append(typeLetterLowerCase);
+            }
+            if (param.HasLetterUpperCase)
+            {
+                builder.Append(typeLetterUpperCase);
+            }
+            if (param.HasCharactor)
+            {
+                builder.Append(typeCharactor);
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/MyPassword/Models/PwdGeneratorParams.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPassword.Models
+{
+    /// <summary>
+    /// 密码生成配置参数
+    /// </summary>
+    public class PwdGeneratorParams
+    {
+        /// <summary>
+        /// 需要数字
+        /// </summary>
+        public bool HasNumber{get;set; }
+        /// <summary>
+        /// 需要小写字母
+        /// </summary>
+        public bool HasLetterLowerCase { get; set; }
+        /// <summary>
+        /// 需要大写字母
+        /// </summary>
+        public bool HasLetterUpperCase { get; set; }
+        /// <summary>
+        /// 需要特殊字符
+        /// </summary>
+        public bool HasCharactor { get; set; }
+
+        public int Length { get; set; }
+
+        public bool IsOk()
+        {
+            if (!HasCharactor && !HasLetterLowerCase && !HasLetterUpperCase && !HasNumber)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
