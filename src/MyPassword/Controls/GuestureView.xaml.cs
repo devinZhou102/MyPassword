d0d4879802999dfc25e887d0ad7cfdb7fb4973b5
周志强 <devin_zhou@outlook.com>
2018-06-02 15:55:23 +0800

    1. Random Phrase

---
+++ src/MyPassword/Controls/GuestureView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyPassword.Controls
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GuestureView : ContentView
+	{
+		public GuestureView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
